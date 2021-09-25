dfb6db6608378e82b18eca4a1bdc5b16a543337b
vicente <enriquefirst@gmail.com>
2021-09-25 09:47:01 -0500

    solo falta la pregunta 10

--- kernel.cu
+++ kernel.cu
@@ -24,7 +24,7 @@ using namespace cv;
 using namespace std;
 
 void get_histogram(Mat img) {
-    imshow("Image", img);
+    imshow("1-Image", img);
     waitKey(0);
     int vector_size = img.rows * img.cols;
     //cout << "rows: " << img.rows << " cols: " << img.cols << endl;
@@ -39,9 +39,9 @@ void get_histogram(Mat img) {
 
     mat_to_vec(img, blue, green, red);
     histogram_cuda(blue, green, red, img.rows, img.cols, output_blue, output_green, output_red);
-    histDisplay(output_blue, "hist blue");
-    histDisplay(output_green, "hist green");
-    histDisplay(output_red, "hist red");
+    histDisplay(output_blue, "1-hist blue");
+    histDisplay(output_green, "1-hist green");
+    histDisplay(output_red, "1-hist red");
 
     /*show_vec(output[0], 256);
     show_vec(output[1], 256);
@@ -64,7 +64,7 @@ void equalize_hist(Mat img) {
     int* new_img_vec = new int[vector_size];
 
        
-    imshow("Original image", grey);
+    imshow("2-Original image", grey);
     waitKey(0);
 
     mat_to_vec(grey, data);
@@ -72,7 +72,7 @@ void equalize_hist(Mat img) {
     
     //histDisplay(hist, "hist");
     Mat new_img = vec_to_mat(new_img_vec, grey.rows, grey.cols);
-    imshow("Image equalized", new_img);
+    imshow("2-Image equalized", new_img);
     waitKey(0);
  
     //show_vec(new_img_vec, 256);
@@ -83,7 +83,7 @@ void aply_function(Mat img) {
     //mat_to_tensor(img, data);
     //show_tensor(data, img.rows, img.cols, img.channels());
 
-    imshow("Image", img);
+    imshow("3-Image", img);
     waitKey(0);
     int vector_size = img.rows * img.cols;
     //cout << "rows: " << img.rows << " cols: " << img.cols << endl;
@@ -101,7 +101,7 @@ void aply_function(Mat img) {
     show_vec(output_blue, 100);
 
     Mat new_img = vec_to_mat(output_blue, output_green, output_red, img.rows, img.cols);
-    imshow("Image global", new_img);
+    imshow("3-Image global", new_img);
     waitKey(0);
 }
 
@@ -127,12 +127,12 @@ void arithmetics(Mat img_1, Mat img_2) {
     Mat img_sub = vec_1d_to_mat(output_3, img_1.rows, img_1.cols);
     Mat img_div = vec_1d_to_mat(output_4, img_1.rows, img_1.cols);
 
-    imshow("Image 1", img_1);    waitKey(0);
-    imshow("Image 2", img_2);    waitKey(0);    
-    imshow("sum", img_sum);    waitKey(0);      
-    imshow("mul", img_mul);    waitKey(0);
-    imshow("sub", img_sub);    waitKey(0);
-    imshow("div", img_div);    waitKey(0);
+    imshow("4-Image 1", img_1);    waitKey(0);
+    imshow("4-Image 2", img_2);    waitKey(0);    
+    imshow("4-sum", img_sum);    waitKey(0);      
+    imshow("4-mul", img_mul);    waitKey(0);
+    imshow("4-sub", img_sub);    waitKey(0);
+    imshow("4-div", img_div);    waitKey(0);
 }
 
 void convolutions(Mat img, float *kernel, int kernel_rows, int kernel_cols) {
@@ -144,8 +144,8 @@ void convolutions(Mat img, float *kernel, int kernel_rows, int kernel_cols) {
     conv_cuda(img_vec, kernel, img.rows, img.cols, img.channels(), kernel_rows, kernel_cols, output);
     Mat img_result = vec_1d_to_mat(output, img.rows, img.cols);    
 
-    imshow("Image original", img);           waitKey(0);
-    imshow("After convolution", img_result);    waitKey(0);
+    imshow("5-Image original", img);           waitKey(0);
+    imshow("5-After convolution", img_result);    waitKey(0);
 }
 
 void zoon(Mat img) {
@@ -197,8 +197,8 @@ void zoon(Mat img) {
     cout << endl;
     show_vec(output, 400);
 
-    imshow("Image low resolution", grey);                    waitKey(0);
-    imshow("Image high resolution", img_result);            waitKey(0);
+    imshow("6-Image low resolution", grey);                    waitKey(0);
+    imshow("6-Image high resolution", img_result);            waitKey(0);
     
 }
 
@@ -217,8 +217,8 @@ void geometrics(Mat img, int* x_1, int* y_1, int* x_2, int* y_2) {
     geometrics_cuda(img_vec, M, img_input.rows, img_input.cols, img_input.channels(), output);
     Mat img_result = vec_1d_to_mat(output, img_input.rows, img_input.cols);
 
-    imshow("Original", img_input);    waitKey(0);
-    imshow("Affine", img_result);     waitKey(0);
+    imshow("7-Original", img_input);    waitKey(0);
+    imshow("7-Affine", img_result);     waitKey(0);
 }
 
 void search_pattern(Mat img, Mat img_pattern) {    
@@ -288,15 +288,15 @@ void search_pattern(Mat img, Mat img_pattern) {
 
    
 
-    imshow("Image original", img_input);    waitKey(0);
-    imshow("Pattern", img_pattern);         waitKey(0);
-    imshow("Patterns finded", new_output_img);  waitKey(0);
+    imshow("8-Image original", img_input);    waitKey(0);
+    imshow("8-Pattern", img_pattern);         waitKey(0);
+    imshow("8-Patterns finded", new_output_img);  waitKey(0);
 }
 
 void optic_flow(Mat img_1, Mat img_2) {
 
-    imshow("Frame 1", img_1);    waitKey(0);
-    imshow("Frame 2", img_2);    waitKey(0);
+    imshow("9-Frame 1", img_1);    waitKey(0);
+    imshow("9-Frame 2", img_2);    waitKey(0);
 
     //optical_flow_analysis(img_1, img_2, 20);
     optical_flow_analysis_cuda(img_1, img_2, 20);
@@ -324,27 +324,27 @@ void optic_flow(Mat img_1, Mat img_2) {
 }
 
 int main() {    
-    // PREGUNTA 1
+    // PREGUNTA 1 /////////////////////////////////////////////////////////////////////////////
     Mat img = imread("D:\\CUDA\\HelloCUDAopenCV\\lena.jpg");
-    //get_histogram(img);    
+    get_histogram(img);    
 
-    // PREGUNTA 2
+    // PREGUNTA 2 /////////////////////////////////////////////////////////////////////////////
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\lena2.jpg");
-    //equalize_hist(img);
+    equalize_hist(img);
 
-    // PREGUNTA 3
+    // PREGUNTA 3 /////////////////////////////////////////////////////////////////////////////
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\lena.jpg");
-    //aply_function(img); 
+    aply_function(img); 
 
     // PREGUNTA 4
     Mat img_1 = imread("D:\\CUDA\\HelloCUDAopenCV\\leon.jpg");
     Mat img_2 = imread("D:\\CUDA\\HelloCUDAopenCV\\aqp.jpg");
     Mat img_3 = imread("D:\\CUDA\\HelloCUDAopenCV\\sub_10.jpg");
     Mat img_4 = imread("D:\\CUDA\\HelloCUDAopenCV\\sub_11.jpg");
-    //arithmetics(img_1, img_2);
-    //arithmetics(img_3, img_4);
+    arithmetics(img_1, img_2);
+    arithmetics(img_3, img_4);
 
-    // PREGUNTA 5
+    // PREGUNTA 5 /////////////////////////////////////////////////////////////////////////////
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\lena.jpg");
     float kernel[] = {
             0.04, 0.04, 0.04, 0.04, 0.04,
@@ -353,7 +353,7 @@ int main() {
             0.04, 0.04, 0.04, 0.04, 0.04,
             0.04, 0.04, 0.04, 0.04, 0.04
     }; // mean
-    //convolutions(img, kernel, 5, 5);
+    convolutions(img, kernel, 5, 5);
 
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\sub_10.jpg");
     float kernel_sobel[] = {
@@ -363,29 +363,28 @@ int main() {
             2, 1, 0, -1, -2,
             2, 1, 0, -1, -2,
     };
-    //convolutions(img, kernel_sobel, 5, 5);
+    convolutions(img, kernel_sobel, 5, 5);
     
 
-    // PREGUNTA 6
+    // PREGUNTA 6 /////////////////////////////////////////////////////////////////////////////
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\orange.jpg");
-    //zoon(img);
+    zoon(img);
 
-    // PREGUNTA 7
+    // PREGUNTA 7 /////////////////////////////////////////////////////////////////////////////
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\orange.jpg");
     int x_1[] = { 0, 0, 10 };   int y_1[] = { 10, 0, 0 };    int x_2[] = { 0, 0, 5 };    int y_2[] = { 5, 0, 0 };
     int x_3[] = { 50, 200, 50 };   int y_3[] = { 50, 50, 200 };    int x_4[] = { 10, 200, 100 };    int y_4[] = { 100, 50, 250 };    
-    //geometrics(img, x_1, y_1, x_2, y_2);
-    //geometrics(img, x_3, y_3, x_4, y_4);
+    geometrics(img, x_1, y_1, x_2, y_2);
+    geometrics(img, x_3, y_3, x_4, y_4);
 
-    // PREGUNTA 8
+    // PREGUNTA 8 /////////////////////////////////////////////////////////////////////////////
     img = imread("D:\\CUDA\\HelloCUDAopenCV\\clouds.jpg");
     Mat img_pattern = imread("D:\\CUDA\\HelloCUDAopenCV\\cloud.jpg");
-    //search_pattern(img, img_pattern);
+    search_pattern(img, img_pattern);
 
-    // PREGUNTA 9
+    // PREGUNTA 9 /////////////////////////////////////////////////////////////////////////////
     img_1 = imread("D:\\CUDA\\HelloCUDAopenCV\\sub_0.jpg");
     img_2 = imread("D:\\CUDA\\HelloCUDAopenCV\\sub_1.jpg");
-
     optic_flow(img_1, img_2);
 
     return 0;
